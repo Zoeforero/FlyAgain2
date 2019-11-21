81fc11a70a5959b9f273e5be5631893aa8ed8b77
Mariana Forero <marianaforerotecano@gmail.com>
2019-11-21 12:09:07 -0500

    Componente buscar vuelos

--- src/containers/Home.jsx
+++ src/containers/Home.jsx
@@ -1,11 +1,20 @@
 import React from 'react';
+import SelectPlace from "../components/SelectPlace";
+import Calendar from "../components/Calendar";
 import Button from '@material-ui/core/Button';
 
 const Home = () => {
   return (
-    <Button variant="contained" color="primary">
-      Hola Mundo!
-    </Button>
+    <section>
+      <h1>Búsqueda Vuelos</h1>
+      <form>
+        <SelectPlace placeholder="¿Desde dónde?"/>
+        <SelectPlace placeholder="¿A dónde quieres ir?"/>
+        <Calendar label="Fecha de salida"/>
+        <Calendar label="Fecha de regreso"/>
+        <Button variant="contained" color="primary" type="submit">Buscar</Button>
+      </form>
+    </section>
   );
 }
 
